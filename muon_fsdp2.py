c746b13cabf2787ae00dfb333930a35ead3bdcfe
ethansmith2000 <ethansmith2000@example.com>
2025-02-06 06:30:12 +0000

    I think this is how you could do it

--- muon_fsdp2.py
+++ muon_fsdp2.py
@@ -111,36 +111,23 @@ class Muon(torch.optim.Optimizer):
         self.rank = rank
         self.world_size = world_size
         defaults = dict(lr=lr, momentum=momentum, nesterov=nesterov, ns_steps=ns_steps)
-        params: list[Tensor] = [*params]
-        param_groups = []
-        for size in {p.numel() for p in params}:
-            b = torch.empty(world_size, size, dtype=torch.bfloat16, device="cuda")
-            group = dict(params=[p for p in params if p.numel() == size],
-                         update_buffer=b, update_buffer_views=[b[i] for i in range(world_size)])
-            param_groups.append(group)
-        super().__init__(param_groups, defaults)
+        super().__init__(params, defaults)
 
     @torch.no_grad()
     def step(self):
         for group in self.param_groups:
-            update_buffer: Tensor = group["update_buffer"]
-            update_buffer_views: list[Tensor] = group["update_buffer_views"]
             # generate weight updates in distributed fashion
             params: list[Tensor] = group["params"]
-            handle = None
-            params_world = None
-            def update_prev(): # optimized Muon implementation contributed by @YouJiacheng
-                handle.wait()
-                for p_world, g_world in zip(params_world, update_buffer_views):
-                    p_world.add_(g_world.view_as(p_world),
-                                 alpha=-group["lr"] * max(1, p_world.size(-2) / p_world.size(-1))**0.5)
 
             for base_i in range(len(params))[::self.world_size]:
                 # every device will be responsible for a different param, but all devices need to share their shards and participate here
                 grads = [None] * self.world_size
+                metas = [None] * self.world_size
                 for i in range(self.world_size):
                     if base_i + i < len(params):
-                        grads[i] = to_local(params[base_i + i].grad, keep_sharded=False)[0]
+                        # every device needs to be around for the gather
+                        grads[i], metas[i] = to_local(params[base_i + i].grad, keep_sharded=False)
+                        # but only one device is responsible for the update, free memory 
                         if not i == self.rank:
                             grads[i] = None
 
@@ -158,13 +145,16 @@ class Muon(torch.optim.Optimizer):
                     if g.ndim == 4: # for the case of conv filters
                         g = g.view(len(g), -1)
                     g = zeropower_via_newtonschulz5(g, steps=group["ns_steps"]).flatten()
-                else:
-                    g = update_buffer_views[self.rank]
+
 
                 # shard the update
+                # first create empty tensor of its dimensions, and then broadcast it to all devices
+                for i in range(self.world_size):
+                    if base_i + i < len(params):
+                        g_world = torch.empty_like(g)
+                        dist.broadcast(g_world, src=self.rank)
+                        # now distribute
+                        g_world = to_dist(g_world, device_mesh=metas[i]["device_mesh"], placements=metas[i]["placements"])
+                        # now do the update
+                        params[base_i + i].add_(g_world, alpha=-group["lr"] * max(1, g.size(-2) / g.size(-1))**0.5)
 
-                if base_i > 0:
-                    update_prev() # async all_gather instead of sync all_reduce by @YouJiacheng
-                handle = dist.all_gather_into_tensor(update_buffer, g, async_op=True)
-                params_world = params[base_i : base_i + self.world_size]
-            update_prev()
